3dfda4d2558ef75776237e44e636f5ca68605092
Tama <mytamhuynhhmt@gmail.com>
2025-08-27 10:15:53 +0700

    route API

---
+++ ExpressJS01/src/routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {creatUer, handleLogin, getUser, getAccount } = require('../controllers/userController');
+const auth = require('../middleware/auth');
+const delay = require('../middleware/delay');
+
+const routerAPI = express.Router();
+
+routerAPI.all('/*', auth);
+
+routerAPI.get('/', (req, res) => {
+    return res.status(200).json("Hello world with API");
+})
+
+routerAPI.post('/register', creatUser);
+routerAPI.post('/login', handleLogin);
+
+routerAPI.get('/users', getUser);
+routerAPI.get('/account', delay, getAccount);
+
+module.exports = routerAPI;
